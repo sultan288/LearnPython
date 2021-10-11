6c5bb567e3d2507b47ed9a22a8bbefeb6cc2e714
sultan288 <sultan.ist28@gmail.com>
2021-10-12 01:04:28 +0600

    update 11.10.21

---
+++ Anisul Islam/program1.py
@@ -0,0 +1,92 @@
+# Python Programming is Starting 10.10.2021
+
+# print("Tipu Sultan")
+# print("01949212011")
+
+# Comment
+# ======================================
+# This is a single line comment
+'''
+This is a 
+multiple
+line comment
+'''
+
+# Backslash character
+# ======================================
+# print("Tipu Sultan \n 011695545644")   # new line
+# print("Tipu Sultan \t 011695545644")
+#
+# print("\"Tipu\"")
+
+# Variable
+# ===========================================
+# name = "Tanvir"
+# age = 23
+# print("Our new student is " + name)
+# print(name + " lives in Dhaka")
+# print("He is currently", age, "lives in Doha")
+# print(name + " is now ", age, " years old")
+#
+# print("TIPU " + "Sultan " + " Tanvir")
+
+# Basic Numerical Operations
+# ==============================================
+# print(11+4)
+# print(11-4)
+# print(11*4)
+# print(11/4)
+# print(11%4)
+#
+# a = 27
+# b = 4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+#
+# print(a//b)  # floor means 4.5~4
+#
+# print(4**3)
+
+# Getting User input
+# =========================================
+
+# name = input("enter name: ")
+# age = input("ENter age: ")
+# gpa = input("ENter gpa: ")
+#
+# print("Student information")
+# print("---------------------")
+# print("Name : "+name)
+# print("age : "+age)
+# print("gpa : "+gpa)
+
+# Type casting
+# ===================================
+
+# num1 = input("Enter first number: ")
+# num2 = input("Enter first number: ")
+#
+# sum = int(num1) + int(num2)
+# print("The sum is ",sum)
+# -------------------------------------------------
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter first number: "))
+#
+# result = num1 + num2
+# print("The result is ", result)
+#
+# result = num1 - num2
+# print("The result is ", result)
+
+# Area of any shape
+# ======================================
+
+# base = float(input("ENter base: "))
+# height = float(input("Enter height: "))
+#
+# Triangle = 0.5 * base * height
+# print("Triangle =",Triangle)
+
